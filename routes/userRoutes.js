e96deb891705624e97921d8542e3e40bfd6e5608
Pedro Augusto <169089519+P-dro97@users.noreply.github.com>
2024-09-19 22:05:41 -0300

    ultimaula

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require ('express');
 const router = express.Router(); //chamar o metodo de rota crinado objeto
 const userController = require('../controllers/userController'); //
+const authenticateToken = require('../middlewares/authMiddlewares');
 
-router.get('/users', userController.listUsers);//
+router.get('/users',authenticateToken, userController.listUsers);//
 
 
 module.exports = router;
